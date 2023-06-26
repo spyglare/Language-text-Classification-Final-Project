4993899a3652842fec1936afadcf43014eb7ddc2
Marcellino Rivaldo Pelaupessy <84267014+spyglare@users.noreply.github.com>
2023-06-27 00:57:12 +0800

    Update Pemrograman_Website_data_tekstual.py

--- Pemrograman_Website_data_tekstual.py
+++ Pemrograman_Website_data_tekstual.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyPpCg4gMqAhLkbLmeUaiMT/",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
